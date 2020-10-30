5bcdda5f261c22051f996477a4b3d07bc2e1799f
Darya Lutuva <lutowa.darya@yandex.ru>
2020-10-30 21:56:35 +0300

    feat: добавить разметку и стили страницы

--- src/App.js
+++ src/App.js
@@ -1,10 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import headerLogo from './images/logo/logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <h1>mesto-react</h1>
+      <div className="page">
+        <header className="header">
+          <img src={headerLogo} alt="логотип" className="logo" />
+        </header>
+        <main className="content">
+          <section className="profile">
+            <img alt="Фото пользователя" className="avatar" />
+            <div className="avatar avatar__hover"></div>
+            <div className="profile-info">
+              <div className="profile-info__container">
+                <h1 className="profile-info__name">Жак-Ив Кусто</h1>
+                <button type="button" className="edit-button">Исследователь океана</button>
+              </div>
+              <p className="profile-info__activity"></p>
+            </div>
+            <button type="button" className="profile__add-button"></button>
+          </section>
+          <section className="places"></section>
+        </main>
+        <footer className="footer">
+          <p className="footer__copyright">&#169; 2020 Mesto Russia</p>
+        </footer>
+        <form className="popup popup_info popup__form" name="user-info" novalidate>
+          <div className="popup__container">
+            <button type="button" className="popup__close-button"></button>
+            <h2 className="popup__title">Редактировать профиль</h2>
+            <fieldset className="popup__profile">
+              <input type="text" placeholder="Имя" className="popup__input popup__input_name" id="name-input" name="name"
+                minlength="2" maxlength="40" required />
+              <span className="popup__error" id="name-input-error"></span>
+              <input type="text" placeholder="О себе" className="popup__input popup__input_activity" id="activity-input"
+                name="about" minlength="2" maxlength="200" required />
+              <span className="popup__error" id="activity-input-error">.</span>
+              <button type="submit" className="popup__button">Сохранить</button>
+            </fieldset>
+          </div>
+        </form>
+        <form className="popup popup_add-card popup__form" name="add-card" novalidate>
+          <div className="popup__container">
+            <button type="button" className="popup__close-button popup__close-button_card"></button>
+            <h2 className="popup__title">Новое место</h2>
+            <fieldset className="popup__profile">
+              <input type="text" placeholder="Название" className="popup__input popup__input_placename" id="placename-input"
+                name="name" minlength="1" maxlength="30" required />
+              <span className="popup__error" id="placename-input-error"></span>
+              <input type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_link" id="email-input"
+                name="link" required />
+              <span className="popup__error" id="email-input-error"></span>
+              <button type="submit" className="popup__button popup__button_card">
+                Сохранить
+          </button>
+            </fieldset>
+          </div>
+        </form>
+        <div className="popup popup_photo">
+          <figure className="popup__photo-container">
+            <button type="button" className="popup__close-button popup__close-button_photo"></button>
+            <div>
+              <img alt="Фотография места" className="popup__image" />
+              <figcaption className="popup__title-photo"></figcaption>
+            </div>
+          </figure>
+        </div>
+        <form className="popup popup__submit">
+          <div className="popup__container">
+            <button type="button" className="popup__close-button"></button>
+            <h2 className="popup__title">Вы уверены?</h2>
+            <button type="submit" className="popup__button">
+              Да
+        </button>
+          </div>
+        </form>
+        <form className="popup popup_avatar popup__form" name="avatar" novalidate>
+          <div className="popup__container">
+            <button type="button" className="popup__close-button"></button>
+            <h2 className="popup__title">Обновить аватар</h2>
+            <fieldset className="popup__profile">
+              <input type="url" placeholder="Ссылка на аватар" className="popup__input popup__input_avatar-link" id="avatar-input"
+                name="avatar" required />
+              <span className="popup__error" id="avatar-input-error"></span>
+              <button type="submit" className="popup__button popup__button_avatar">
+                Сохранить
+        </button>
+            </fieldset>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
