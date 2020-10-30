6ce4b509bc882a2e2fd7e52681904dcd6a3d10ab
Darya Lutuva <lutowa.darya@yandex.ru>
2020-10-31 01:18:07 +0300

    feat: создание компонента PopupWithForm для попапов форм

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
   return (
@@ -9,39 +10,8 @@ function App() {
         <Header />
         <Main />
         <Footer />
-        <form className="popup popup_info popup__form" name="user-info" novalidate>
-          <div className="popup__container">
-            <button type="button" className="popup__close-button"></button>
-            <h2 className="popup__title">Редактировать профиль</h2>
-            <fieldset className="popup__profile">
-              <input type="text" placeholder="Имя" className="popup__input popup__input_name" id="name-input" name="name"
-                minlength="2" maxlength="40" required />
-              <span className="popup__error" id="name-input-error"></span>
-              <input type="text" placeholder="О себе" className="popup__input popup__input_activity" id="activity-input"
-                name="about" minlength="2" maxlength="200" required />
-              <span className="popup__error" id="activity-input-error">.</span>
-              <button type="submit" className="popup__button">Сохранить</button>
-            </fieldset>
-          </div>
-        </form>
-        <form className="popup popup_add-card popup__form" name="add-card" novalidate>
-          <div className="popup__container">
-            <button type="button" className="popup__close-button popup__close-button_card"></button>
-            <h2 className="popup__title">Новое место</h2>
-            <fieldset className="popup__profile">
-              <input type="text" placeholder="Название" className="popup__input popup__input_placename" id="placename-input"
-                name="name" minlength="1" maxlength="30" required />
-              <span className="popup__error" id="placename-input-error"></span>
-              <input type="url" placeholder="Ссылка на картинку" className="popup__input popup__input_link" id="email-input"
-                name="link" required />
-              <span className="popup__error" id="email-input-error"></span>
-              <button type="submit" className="popup__button popup__button_card">
-                Сохранить
-          </button>
-            </fieldset>
-          </div>
-        </form>
-        <div className="popup popup_photo">
+        <PopupWithForm />
+        {/* <div className="popup popup_photo">
           <figure className="popup__photo-container">
             <button type="button" className="popup__close-button popup__close-button_photo"></button>
             <div>
@@ -49,30 +19,8 @@ function App() {
               <figcaption className="popup__title-photo"></figcaption>
             </div>
           </figure>
-        </div>
-        <form className="popup popup__submit">
-          <div className="popup__container">
-            <button type="button" className="popup__close-button"></button>
-            <h2 className="popup__title">Вы уверены?</h2>
-            <button type="submit" className="popup__button">
-              Да
-        </button>
-          </div>
-        </form>
-        <form className="popup popup_avatar popup__form" name="avatar" novalidate>
-          <div className="popup__container">
-            <button type="button" className="popup__close-button"></button>
-            <h2 className="popup__title">Обновить аватар</h2>
-            <fieldset className="popup__profile">
-              <input type="url" placeholder="Ссылка на аватар" className="popup__input popup__input_avatar-link" id="avatar-input"
-                name="avatar" required />
-              <span className="popup__error" id="avatar-input-error"></span>
-              <button type="submit" className="popup__button popup__button_avatar">
-                Сохранить
-        </button>
-            </fieldset>
-          </div>
-        </form>
+        </div> */}
+
       </div>
     </div>
   );
